fae29b7cbdf6115a3ab0d5ec064e9086308dfec0
柠檬冲水 <916415899@qq.com>
2023-04-14 23:29:48 +0800

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,8 +0,0 @@
-const { join } = require('path')
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // 下载到项目根目录 适配pnpm
-  downloadPath: join(__dirname, '.cache', 'puppeteer')
-}
