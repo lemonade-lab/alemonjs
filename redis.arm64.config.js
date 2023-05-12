97331a72f1d96daf79fadd3f3ade4b7dc9a18730
916415899@qq.com <916415899@qq.com>
2023-05-12 17:34:44 +0800

    redis特化处理

--- redis.arm64.config.js
+++ redis.arm64.config.js
@@ -4,6 +4,6 @@ module.exports = {
   //如果是windows对应的应用C:\\Program Files\\Redis\\redis-server.exe
   //但windows更推荐使用win服务以自动开启服务
   script: 'redis-server',
-  args: ['--port', '6379', '--bind', '127.0.0.1'],
+  args: ['--port', '6379', '--bind', '127.0.0.1', '--ignore-warnings', 'ARM64-COW-BUG'],
   autorestart: true
 }
