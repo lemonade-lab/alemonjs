d246f359d1ebd213affa9e952e65faaf094369d2
916415899@qq.com <916415899@qq.com>
2023-05-13 00:10:27 +0800

    增加配置提示

--- app.config.ts
+++ app.config.ts
@@ -4,10 +4,11 @@ const Rcf = {
   port: 6379
 }
 
-/** 机器人登录配置文件 */
-const Bcf = '/config/config.yaml'
-/** 机器人登录配置文件 */
+/** 机器人登录默认配置文件 */
 const Dcf = '/src/config/config_default.yaml'
+
+/** 机器人登录启动配置文件 */
+const Bcf = '/config/config.yaml'
 
 /** 环境配置 */
 const Acf = {
@@ -19,6 +20,13 @@ const Acf = {
 const PuPcf = {
   //浏览器路径
   chromePath: ''
+  /**
+   * 因部分架构不同可自行配置以正常使用截图功能
+   * 如 C:/Users/alemon/Desktop/alemon-bot/.cache/puppeteer/win64-1108766/chrome-win/chrome.exe
+   * C:/Users/alemon/Desktop/alemon-bot                   是机器人的根目录
+   *.cache/puppeteer/win64-1108766/chrome-win/chrome.exe  是起步时候下载好的浏览器
+   * 如果没有下载浏览器,可根据自己的需求下载并配置
+   */
 }
 
 /* 测试插件开关 */
