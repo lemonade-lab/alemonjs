94be400cd0eb4c52b6e9edbc0b39d67002e7f73f
柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>
2023-05-15 23:58:57 +0000

    update src/db/redis/index.ts.
    Signed-off-by: 柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>

--- src/db/redis/index.ts
+++ src/db/redis/index.ts
@@ -1,15 +1,17 @@
 import redisClient, { Redis } from 'ioredis'
 import { Rcf } from '../../../app.config'
 declare global {
-  //数据库对象
-  var redis: Redis
+    //数据库对象
+    var redis: Redis
 }
 const redis = new redisClient({ ...Rcf })
 global.redis = redis
 export const ctreateRedis = () => {
-  redis.on('error', error => {
-    console.error('[REDIS]', error)
-    process.exit()
-  })
-  console.info('[REIDS] 连接成功~')
+    redis.on('connect', () => {
+    console.info('[REIDS] 连接成功~')
+    })
+    redis.on('error', error => {
+        console.error('[REDIS]', error)
+        process.exit()
+    })
 }
