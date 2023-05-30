5ecaa777fa830428a1fab403408113cd20cb1156
柠檬冲水 <916415899@qq.com>
2023-05-30 14:16:25 +0800

    优化

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -3,6 +3,7 @@ const { join } = require('path')
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
+  skipDownload: true,
   //适配pnpm
   downloadPath: join(process.cwd(), '.cache', 'puppeteer')
 }
