f25a4131fbdb369f237624bea809426eb568c245
柠檬冲水 <916415899@qq.com>
2023-06-15 12:44:21 +0800

    修复配置读取错误

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,10 +6,12 @@ export const defaultConfigLoginPath = 'config_default/login.yaml'
 export const configLoginPath = 'config/login.yaml'
 export const defaultConfigPath = 'config_default/app.yaml'
 export const configPath = 'config/app.yaml'
+/* 检查应用配置 */
 if (existsSync(defaultConfigPath) && !existsSync(configPath)) {
   cpSync(defaultConfigPath, configPath, {
     recursive: true
   })
 }
-const AppConfig: ConfigType = getYaml(join(process.cwd(), defaultConfigPath))
+/* 读取生成后的配置 */
+const AppConfig: ConfigType = getYaml(join(process.cwd(), configPath))
 export { AppConfig }
