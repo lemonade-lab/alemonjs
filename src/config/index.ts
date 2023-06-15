8edfb6eb2a596a198d3611176147cf5b89d4929a
柠檬冲水 <916415899@qq.com>
2023-06-15 20:05:50 +0800

    增加mysql通用配置

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,13 +3,21 @@ import { join } from 'path'
 import { existsSync, cpSync } from 'fs'
 export const DefaultConfigLogin = 'config_default/login.yaml'
 export const DefaultConfigRedis = 'config_default/redis.yaml'
+export const DefaultConfigMysql = 'config_default/mysql.yaml'
 export const DefaultConfigPuppeteer = 'config_default/puppeteer.yaml'
 export const ConfigLogin = 'config/login.yaml'
 export const ConfigRedis = 'config/redis.yaml'
+export const ConfigMysql = 'config/mysql.yaml'
 export const ConfigPuppeteer = 'config/puppeteer.yaml'
 /** redis  */
 if (existsSync(DefaultConfigRedis) && !existsSync(ConfigRedis)) {
   cpSync(DefaultConfigRedis, ConfigRedis, {
+    recursive: true
+  })
+}
+/** mysql  */
+if (existsSync(DefaultConfigMysql) && !existsSync(ConfigMysql)) {
+  cpSync(DefaultConfigMysql, ConfigMysql, {
     recursive: true
   })
 }
@@ -19,7 +27,6 @@ if (existsSync(DefaultConfigPuppeteer) && !existsSync(ConfigPuppeteer)) {
     recursive: true
   })
 }
-/** 读取配置 */
-const RedisConfig: any = getYaml(join(process.cwd(), ConfigRedis))
+/* 读取配置 */
 const PuppeteerConfig: any = getYaml(join(process.cwd(), ConfigPuppeteer))
-export { RedisConfig, PuppeteerConfig }
+export { PuppeteerConfig }
