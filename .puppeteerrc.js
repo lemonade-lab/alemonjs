2031d9bb0ca12a298f7b782ab07ecfb89f422de9
柠檬冲水 <916415899@qq.com>
2023-06-17 20:08:09 +0800

    增加架构

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -1,6 +1,12 @@
+const os = require('os')
+let skipDownload = false
+let arch = os.arch()
+if (arch == 'arm64' || arch == 'aarch64') {
+  skipDownload = true
+}
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  skipDownload: true
+  skipDownload
 }
