401ac978799427c82cd9f7ae94666abaf795f94f
柠檬冲水 <916415899@qq.com>
2023-06-26 22:21:41 +0800

    升级V1.4

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -9,18 +9,20 @@ if (process.platform == 'win32') {
     // 存在 Edge
     skipDownload = true
     executablePath = 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
-    console.log('[Edge] start ~')
+    console.info('[win32] system')
+    console.info('[Edge] start')
   }
 } else if (process.platform == 'linux') {
   //如果arm64架构跳过下载
   if (arch == 'arm64' || arch == 'aarch64') {
+    console.info('[arm64/aarch64] system')
     skipDownload = true
   }
   //不管什么架构,如果存在配置则跳过下载,且配置路径
   if (existsSync('/usr/bin/chromium')) {
     skipDownload = true
     executablePath = '/usr/bin/chromium'
-    console.log('[chromium] start ~')
+    console.info('[Chromium] start')
   }
 }
 
