efb1608ac413c0f7c3b744d2175e9124cd336aa1
空巷一人 <1530688385@qq.com>
2023-06-27 07:47:47 +0000

    update index.ts.
    Signed-off-by: 空巷一人 <1530688385@qq.com>

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import { createAlemon } from 'alemon-qq'
 createAlemon()
 
 // 监听退出,防止ts-node 退出报错
-process.on('SIGINT', signals => console.log(signals))
+process.on('SIGINT', signals => {
+  console.log(signals)
+  if (process.pid) {
+    process.exit()
+  }
+})
