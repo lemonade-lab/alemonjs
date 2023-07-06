d9125819b81045833134cf598406921487e8960c
柠檬冲水 <916415899@qq.com>
2023-07-06 22:41:27 +0800

    更改环境

--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
-import { createAlemon } from 'alemon-qq'
+import { createAlemon } from './src/index.js'
+/** 创建机器人 */
 await createAlemon().catch(err => {
-  console.log('启动失败~', err)
-  return
-})
-
-// 监听退出,防止ts-node 退出报错
-process.on('SIGINT', signals => {
-  console.log(signals)
-  if (process.pid) {
-    process.exit()
-  }
-  return
+  console.log(err)
+  return false
 })
