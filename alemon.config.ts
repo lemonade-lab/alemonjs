da901dcd6de22a1d2d44175c9466b4a599b31ca3
柠檬冲水 <916415899@qq.com>
2023-10-31 18:03:13 +0800

    更新

--- alemon.config.ts
+++ alemon.config.ts
@@ -1,9 +1,6 @@
 import { defineAlemonConfig } from './src/index.js'
 defineAlemonConfig({
   login: {
-    one: {
-      url: 'ws://bots.liucl.cn/717505091/V12',
-      access_token: 'll2339623'
-    }
+    one: {}
   }
 })
