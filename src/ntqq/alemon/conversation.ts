0ecb2bb8ae009e74622680d2047862297104e6e5
柠檬冲水 <916415899@qq.com>
2023-11-22 17:38:05 +0800

    更新

--- src/ntqq/alemon/conversation.ts
+++ src/ntqq/alemon/conversation.ts
@@ -15,9 +15,9 @@ export function conversation(t: string, d: any) {
       avatar: 'string'
     })
   } else if (t == 'GROUP_AT_MESSAGE_CREATE') {
-    C2C_MESSAGE_CREATE(d)
-  } else if (t == 'C2C_MESSAGE_CREATE') {
     GROUP_AT_MESSAGE_CREATE(d)
+  } else if (t == 'C2C_MESSAGE_CREATE') {
+    C2C_MESSAGE_CREATE(d)
   } else {
     console.log(t)
     console.log(d)
