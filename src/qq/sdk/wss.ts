ed90cc11353b8ffbc5853232aa71bc68d10edd96
柠檬冲水 <916415899@qq.com>
2024-01-11 16:32:07 +0800

    增加qq平台短线重拉

--- src/qq/sdk/wss.ts
+++ src/qq/sdk/wss.ts
@@ -3,6 +3,7 @@ import { ClientQQ } from './api.js'
 import { config } from './config.js'
 import { getIntentsMask } from './intents.js'
 import { IntentsEnum } from './typings.js'
+import { Counter } from '../../core/index.js'
 
 /**
  * ******
@@ -65,38 +66,41 @@ export const defineQQ: QQOptions = {
 }
 
 /**
- * 客户端
+ * 连接
  */
 export class Client {
-  /**
-   * 连接地址
-   */
-  #url = null
-  /**
-   * 标记是否已连接
-   */
+  //
+  #counter = new Counter(1) // 初始值为1
+
+  // 标记是否已连接
   #isConnected = false
-  /**
-   * 周期
-   */
+
+  // 存储最新的消息序号
   #heartbeat_interval = 30000
+
+  // 鉴权
+  #IntervalID = null
+
+  // url
+  #gatewayUrl = null
 
   /**
    * 设置配置
+   * @param opstion
    */
-  set(qq: QQOptions) {
-    config.set('appID', qq.appID)
-    config.set('token', qq.token)
-    config.set('intents', qq.intents)
-    config.set('sandbox', qq.sandbox)
-    config.set('shard', qq.shard)
+  set(opstion: QQOptions) {
+    config.set('appID', opstion.appID)
+    config.set('token', opstion.token)
+    config.set('intents', opstion.intents)
+    config.set('sandbox', opstion.sandbox)
+    config.set('shard', opstion.shard)
   }
 
   /**
-   * 得到鉴权配置
+   * 鉴权数据
    * @returns
    */
-  #art() {
+  #aut() {
     const appID = config.get('appID')
     const token = config.get('token')
     const intents = config.get('intents')
@@ -119,86 +123,115 @@ export class Client {
   #ws: WebSocket
 
   /**
-   * opcode
    *
-   * s 标识消息的唯一性
-   *
-   * t 代表事件类型
-   *
-   * d 代表事件内容
+   * @param cfg
+   * @param conversation
    */
+  async connect(conversation: (...args: any[]) => any) {
+    this.#gatewayUrl = await ClientQQ.geteway().then(res => res.url)
 
-  /**
-   * 建立 WebSocket 连接
-   * @param callBack
-   * @param shard
-   */
-  async connect(callBack: (...args: any[]) => any) {
-    this.#url = await ClientQQ.geteway().then(res => res.url)
-    if (!this.#url) return
+    // 请求url
+    if (!this.#gatewayUrl) return
 
-    const map = {
-      0: ({ d, t }) => {
-        callBack(t, d)
-        if (t === 'READY') {
-          // Ready Event，鉴权成功
-          setInterval(() => {
-            /**
-             * 心跳定时发送
-             */
-            if (this.#isConnected) {
-              this.#ws.send(
-                JSON.stringify({
-                  op: 1, //  op = 1
-                  d: null // 如果是第一次连接，传null
-                })
-              )
-            }
-          }, this.#heartbeat_interval)
-        } else if (t === 'RESUMED') {
-          // Resumed Event，恢复连接成功
-        } else {
-          // 处理不同类型的事件内容
-        }
-      },
-      7: message => {
-        console.info(message)
-      },
-      9: message => {
-        console.info(message)
-      },
-      10: ({ d }) => {
-        // OpCode 10 Hello 消息，处理心跳周期
-        this.#isConnected = true
-        this.#heartbeat_interval = d.heartbeat_interval
-        /**
-         * 发送鉴权
-         */
-        this.#ws.send(JSON.stringify(this.#art()))
-      },
-      11: message => {
-        console.info('[ws] heartbeat transmission')
-      },
-      12: message => {
-        console.info(message)
+    // 重新连接的逻辑
+    const reconnect = async () => {
+      if (this.#counter.get() >= 5) {
+        console.info(
+          'The maximum number of reconnections has been reached, cancel reconnection'
+        )
+        return
       }
+      setTimeout(() => {
+        console.info('[ws] reconnecting...')
+        // 重新starrt
+        start()
+        // 记录
+        this.#counter.getNextID()
+      }, 5000)
     }
 
-    this.#ws = new WebSocket(this.#url)
-
-    this.#ws.on('open', () => {
-      console.info('[ws] open')
-    })
-
-    this.#ws.on('message', msg => {
-      const message = JSON.parse(msg.toString('utf8'))
-      // 根据 opcode 进行处理
-      if (process.env.QQ_WS == 'dev') console.info('message', message)
-      if (map[message.op]) map[message.op](message)
-    })
-
-    this.#ws.on('close', () => {
-      console.error('[ws]  close')
-    })
+    const start = () => {
+      if (this.#gatewayUrl) {
+        const map = {
+          0: async ({ t, d }) => {
+            // 存在,则执行t对应的函数
+            await conversation(t, d)
+            // Ready Event，鉴权成功
+            if (t === 'READY') {
+              this.#IntervalID = setInterval(() => {
+                if (this.#isConnected) {
+                  this.#ws.send(
+                    JSON.stringify({
+                      op: 1, //  op = 1
+                      d: null // 如果是第一次连接，传null
+                    })
+                  )
+                }
+              }, this.#heartbeat_interval)
+            }
+            // Resumed Event，恢复连接成功
+            if (t === 'RESUMED') {
+              console.info('[ws] restore connection')
+              // 重制次数
+              this.#counter.reStart()
+            }
+            return
+          },
+          6: ({ d }) => {
+            console.info('[ws] connection attempt', d)
+            return
+          },
+          7: async ({ d }) => {
+            // 执行重新连接
+            console.info('[ws] reconnect', d)
+            // 取消鉴权发送
+            if (this.#IntervalID) clearInterval(this.#IntervalID)
+            return
+          },
+          9: ({ d, t }) => {
+            console.info('[ws] parameter error', d)
+            return
+          },
+          10: ({ d }) => {
+            // 重制次数
+            this.#isConnected = true
+            // 记录新循环
+            this.#heartbeat_interval = d.heartbeat_interval
+            // 发送鉴权
+            this.#ws.send(JSON.stringify(this.#aut()))
+            return
+          },
+          11: () => {
+            // OpCode 11 Heartbeat ACK 消息，心跳发送成功
+            console.info('[ws] heartbeat transmission')
+            // 重制次数
+            this.#counter.reStart()
+            return
+          },
+          12: ({ d }) => {
+            console.info('[ws] platform data', d)
+            return
+          }
+        }
+        // 连接
+        this.#ws = new WebSocket(this.#gatewayUrl)
+        this.#ws.on('open', () => {
+          console.info('[ws] open')
+        })
+        // 监听消息
+        this.#ws.on('message', async msg => {
+          const message = JSON.parse(msg.toString('utf8'))
+          if (process.env.NTQQ_WS == 'dev') console.info('message', message)
+          // 根据 opcode 进行处理
+          if (map[message.op]) await map[message.op](message)
+        })
+        // 关闭
+        this.#ws.on('close', async err => {
+          await reconnect()
+          console.info('[ws] close', err)
+        })
+      }
+    }
+    start()
   }
 }
