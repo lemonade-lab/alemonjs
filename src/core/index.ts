81d38e6f6a1cb65b5f9fa8e648d482cdec842797
柠檬冲水 <916415899@qq.com>
2024-01-11 16:03:52 +0800

    清除错误导出

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,10 +3,10 @@
  * private
  * ********
  */
-export * from './processor/configs.js'
 export * from './typings.js'
-export * from './utils/index.js'
 export * from './types.js'
+export * from './processor/configs.js'
 export * from './processor/path.js'
 export * from './processor/application.js'
 export * from './processor/log.js'
+export * from './utils/index.js'
--- src/core/main.ts
+++ src/core/main.ts
@@ -3,12 +3,12 @@
  * public
  * ********
  */
-export * from './processor/configs.js'
-export * from './processor/help.js'
-export * from './index.js'
-export * from './utils/index.js'
 export * from './typings.js'
 export * from './types.js'
+/**
+ * *****
+ * *****
+ */
 export * from './processor/alemon.js'
 export * from './processor/path.js'
 export * from './processor/help.js'
@@ -16,3 +16,7 @@ export * from './processor/plugin.js'
 export * from './processor/subscribe.js'
 export * from './processor/application.js'
 export * from './processor/configs.js'
+/**
+ *
+ */
+export * from './utils/index.js'
