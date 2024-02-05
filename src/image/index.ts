878753dc84547949638bbc2872198aa53403645c
柠檬冲水 <916415899@qq.com>
2024-02-05 17:05:11 +0800

    自动下载选项地址

--- src/image/index.ts
+++ src/image/index.ts
@@ -1,11 +1,13 @@
-import { mkdirSync, writeFileSync } from 'node:fs'
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
 import { dirname, join } from 'node:path'
-import { readFileSync, cpSync } from 'node:fs'
+import { readFileSync } from 'node:fs'
 import { Puppeteer } from '../core/index.js'
 import { type ScreenshotFileOptions } from '../core/index.js'
 import { getStrMatchSize, replaceLocal } from './utils.js'
 import { Cache } from './html.js'
 import { PuppeteerLaunchOptions } from 'puppeteer'
+import axios from 'axios'
+
 /**
  * 插入模板
  * @param str
@@ -20,28 +22,82 @@ function render(str: string, arr: { reg: RegExp; val: string }[]) {
   return str
 }
 
+type ImageType = {
+  cwd?: string
+  paths?: {
+    [key: string]: string
+    'global.js'?: string
+  }
+}
+
+const GlobalUrl =
+  process.env?.ALEMONJS_IMAGE_GLOVAL_JS ??
+  'https://unpkg.com/vue@3/dist/vue.global.js'
+
 /**
  * 创建图片对象
  * @param cwd
  */
-export function createImage(cwd?: string) {
-  // 传进来,设置一些参数
-  let ImageCwd = (cwd ?? process.cwd()).replace(/\\/g, '/')
+export function createImage(optoins?: string | ImageType) {
+  if (typeof optoins == 'string') {
+    // 普通模式
+    const optoin = {
+      cwd: optoins,
+      paths: {
+        'global.js': GlobalUrl
+      }
+    }
+    // 替换为新模式
+    optoins = optoin
+  }
+  if (!optoins) {
+    optoins = {
+      cwd: process.cwd().replace(/\\/g, '/'),
+      paths: {
+        'global.js': GlobalUrl
+      }
+    }
+  }
+  if (!optoins?.cwd) {
+    // 不存在
+    optoins['cwd'] = process.cwd().replace(/\\/g, '/')
+  }
+  if (!optoins?.paths) {
+    optoins.paths = {
+      'global.js': GlobalUrl
+    }
+  }
+  if (!optoins?.paths?.['global.js']) {
+    optoins.paths['global.js'] = GlobalUrl
+  }
 
-  const im = join(ImageCwd, '.image')
-  const y = join(process.cwd(), '.image')
+  const cwd = join(process.cwd(), '.image')
 
   // 确保目录存在
-  mkdirSync(im, { recursive: true })
+  mkdirSync(cwd, { recursive: true })
 
-  // 同步.image文件
-  if (y != im) cpSync(im, y, { recursive: true })
+  const promises = []
+  for (const item in optoins.paths) {
+    const dir = join(cwd, item)
+    // 不存在文件
+    if (!existsSync(dir)) {
+      promises.push(
+        axios({
+          url: optoins.paths[item],
+          timeout: 6 * 1000 // 6s
+        })
+          .then(res => res.data)
+          .then(res => {
+            writeFileSync(dir, res)
+          })
+      )
+    }
+  }
+  Promise.all(promises).catch(console.error)
 
   let ImageLate: string
 
   let ImageHtml: string
-
-  let ImageOptions: ScreenshotFileOptions
 
   // 实例化
   const pup = new Puppeteer()
@@ -69,8 +125,8 @@ export function createImage(cwd?: string) {
      * @param cwd
      * @returns
      */
-    cwd: (cwd?: string) => {
-      ImageCwd = (cwd ?? process.cwd()).replace(/\\/g, '/')
+    cwd: (cwd: string) => {
+      if (typeof optoins != 'string') optoins.cwd = cwd.replace(/\\/g, '/')
       return app
     },
     /**
@@ -85,74 +141,77 @@ export function createImage(cwd?: string) {
       data?: any
       cance?: boolean
     }) => {
-      /**
-       * ********
-       * 字符串解析 - 同时替换@ - 插入模板
-       * ********
-       */
-      const html = render(Cache.get(), [
-        {
-          reg: /<ImageStyle\s*\/>/,
-          // 提取 替换
-          val: getStrMatchSize(
-            ImageCwd,
-            str,
-            /<\s*style\s*>([\s\S]*?)<\/style\s*>/,
-            0,
-            cance
-          )
-        },
-        {
-          reg: /<ImageHead\s*\/>/,
-          // 提取 替换
-          val: getStrMatchSize(
-            ImageCwd,
-            str,
-            /<\s*head\s*>([\s\S]*?)<\/head\s*>/,
-            1,
-            cance
-          )
-        },
-        {
-          reg: /<ImageTemplate\s*\/>/,
-          // 提取 替换
-          val: getStrMatchSize(
-            ImageCwd,
-            str,
-            /<\s*template\s*>([\s\S]*?)<\/template\s*>/,
-            1,
-            cance
-          )
-        },
-        {
-          reg: /<ImageScript\s*\/>/,
-          // 提取 替换
-          val: getStrMatchSize(
-            ImageCwd,
-            str,
-            /<\s*script\s*>([\s\S]*?)<\/script\s*>/,
-            0,
-            cance
-          )
-        },
-        {
-          reg: /<ImageData\s*\/>/,
-          // 插入数据
-          val: JSON.stringify(data)
-        },
-        {
-          reg: /<ImageDir\s*\/>/,
-          // 插入地址
-          val: ImageCwd
-        }
-      ])
+      if (typeof optoins != 'string') {
+        /**
+         * ********
+         * 字符串解析 - 同时替换@ - 插入模板
+         * ********
+         */
+        const html = render(Cache.get(), [
+          {
+            reg: /<ImageStyle\s*\/>/,
+            // 提取 替换
+            val: getStrMatchSize(
+              optoins.cwd,
+              str,
+              // 可能是多个style标签
+              /<\s*(style|style\s*scoped)\s*>([\s\S]*?)<\/style\s*>/,
+              0,
+              cance
+            )
+          },
+          {
+            reg: /<ImageHead\s*\/>/,
+            // 提取 替换
+            val: getStrMatchSize(
+              optoins.cwd,
+              str,
+              /<\s*head\s*>([\s\S]*?)<\/head\s*>/,
+              1,
+              cance
+            )
+          },
+          {
+            reg: /<ImageTemplate\s*\/>/,
+            // 提取 替换
+            val: getStrMatchSize(
+              optoins.cwd,
+              str,
+              /<\s*template\s*>([\s\S]*?)<\/template\s*>/,
+              1,
+              cance
+            )
+          },
+          {
+            reg: /<ImageScript\s*\/>/,
+            // 提取 替换
+            val: getStrMatchSize(
+              optoins.cwd,
+              str,
+              /<\s*script\s*>([\s\S]*?)<\/script\s*>/,
+              0,
+              cance
+            )
+          },
+          {
+            reg: /<ImageData\s*\/>/,
+            // 插入数据
+            val: JSON.stringify(data)
+          },
+          {
+            reg: /<ImageDir\s*\/>/,
+            // 插入地址
+            val: optoins.cwd
+          }
+        ])
 
-      /**
-       * ********
-       * 解析 替换 ~
-       * ********
-       */
-      ImageHtml = replaceLocal(html, cance)
+        /**
+         * ********
+         * 解析 替换 ~
+         * ********
+         */
+        ImageHtml = replaceLocal(html, cance)
+      }
 
       return app
     },
@@ -170,31 +229,30 @@ export function createImage(cwd?: string) {
       data?: any
       cance?: boolean
     }) => {
-      ImageLate = late
-      // 字符串读取
-      const str = readFileSync(join(ImageCwd, ImageLate), 'utf-8')
-      // 渲染html
-      app.Render({
-        str,
-        data,
-        cance
-      })
+      if (typeof optoins != 'string') {
+        ImageLate = late
+        // 字符串读取
+        const str = readFileSync(join(optoins.cwd, ImageLate), 'utf-8')
+        // 渲染html
+        app.Render({
+          str,
+          data,
+          cance
+        })
+      }
       return app
     },
     /**
      * 获取html
      * @returns
      */
-    getHtml: () => {
-      return ImageHtml
-    },
+    getHtml: () => ImageHtml,
     /**
      * 截图
-     * @param options
+     * @param select
      * @returns
      */
-    screenshot: (options?: ScreenshotFileOptions) => {
-      ImageOptions = options
+    screenshot: (select?: ScreenshotFileOptions) => {
       // 写入地址 项目根目录 + 'data' + 文件地址.html
       const directory = join(process.cwd(), 'data', `${ImageLate}.html`)
       // 确保写入目录存在
@@ -202,9 +260,9 @@ export function createImage(cwd?: string) {
       // 写入文件
       writeFileSync(directory, ImageHtml)
       return pup.toFile(directory, {
-        SOptions: ImageOptions?.SOptions ?? { type: 'jpeg', quality: 90 },
-        tab: ImageOptions?.tab ?? 'body',
-        timeout: ImageOptions?.timeout ?? 120000
+        SOptions: select?.SOptions ?? { type: 'jpeg', quality: 90 },
+        tab: select?.tab ?? 'body',
+        timeout: select?.timeout ?? 120000
       })
     }
   }
