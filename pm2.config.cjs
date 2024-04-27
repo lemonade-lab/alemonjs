c3f4c05bd51a734c5f2fd0bcb3439a65e8c832f0
ningmengchongshui <916415899@qq.com>
2024-04-27 19:46:51 +0800

    feat: 新增pm2

---
+++ pm2.config.cjs
@@ -0,0 +1,60 @@
+const ars = [...process.argv.slice(4)]
+const BotName = 'alemonb'
+const findArs = ars.find(item => item.startsWith('@'))
+const apps = []
+if (!findArs) {
+  apps.push({
+    name: BotName,
+    args: ars
+  })
+} else {
+  if (!ars[0].startsWith('@')) ars.unshift(BotName)
+  const msg = ars.join(' ')
+  const arr = msg.split('@')
+  for (const arg of arr) {
+    if (arg == '') continue
+    const ar = arg.split(' ')
+    const existingApp = apps.find(app => app.name === ar[0])
+    if (!existingApp) {
+      const name = ar.shift()
+      if (name != undefined) {
+        apps.push({
+          name: name,
+          args: ar ?? []
+        })
+      }
+    }
+  }
+}
+console.log('------------------------------------------')
+module.exports = {
+  apps: apps.map(app => ({
+    name: app.name,
+    script: 'node_modules/alemonjs/bin/index.js',
+    instances: 1,
+    autorestart: true,
+    exec_mode: 'cluster',
+    max_memory_restart: '2G',
+    cron_restart: '0 */2 * * *',
+    args: (() => {
+      app.args.push('run')
+      return app.args
+    })(),
+    watch: false,
+    autodump: true,
+    merge_logs: true,
+    error_file: `./logs/${app.name}/err.log`,
+    out_file: `./logs/${app.name}/out.log`,
+    log_max_size: '10M',
+    log_rotate_interval: 'daily',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    env: {
+      NODE_ENV: 'production'
+    },
+    kill_timeout: 5000,
+    listen_timeout: 3000,
+    max_restarts: 10,
+    restart_delay: 5000,
+    restart_delay_max: 10000
+  }))
+}
