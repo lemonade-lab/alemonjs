5a8ab73235156b0da59e67c6302822bdaa37e6f3
ningmengchongshui <916415899@qq.com>
2024-05-04 01:44:40 +0800

    feat: 修复pm2无法出记录

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -31,27 +31,38 @@ module.exports = {
   apps: apps.map(app => ({
     name: app.name,
     script: 'node_modules/alemonjs/bin/index.js',
-    instances: 1,
-    autorestart: true,
-    exec_mode: 'cluster',
-    max_memory_restart: '2G',
-    cron_restart: '0 */2 * * *',
+    // exec_mode: 'cluster',
     args: app.args,
-    watch: false,
-    autodump: true,
-    merge_logs: true,
     error_file: `./logs/${app.name}/err.log`,
-    out_file: `./logs/${app.name}/out.log`,
-    log_max_size: '10M',
-    log_rotate_interval: 'daily',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    env: {
-      NODE_ENV: 'production'
-    },
-    kill_timeout: 5000,
-    listen_timeout: 3000,
-    max_restarts: 10,
-    restart_delay: 5000,
-    restart_delay_max: 10000
-  }))
+    out_file: `./logs/${app.name}/out.log`
+  })),
+  // 是否将所有进程的日志合并到一个文件中
+  merge_logs: true,
+  // 达到这个大小后日志文件进行轮转
+  log_max_size: '10M',
+  // 日志文件每天会被轮转一次
+  log_rotate_interval: 'daily',
+  // 日志格式
+  log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  env: {
+    NODE_ENV: 'production'
+  },
+  // 将 PM2 进程列表自动保存到文件中
+  autodump: true,
+  // 不监听文件变化
+  watch: false,
+  // 超时时间内进程仍未终止，则 PM2 将强制终止该进程
+  kill_timeout: 5000,
+  // 发送意外重启
+  autorestart: true,
+  // 进程到达指定内存时重启
+  max_memory_restart: '2G',
+  // 重启规则
+  cron_restart: '0 */2 * * *',
+  // 最大重启数
+  max_restarts: 5,
+  // 进程重启之间的延迟时间
+  restart_delay: 5000,
+  // 进程重启之间的最大延迟时间
+  restart_delay_max: 10000
 }
