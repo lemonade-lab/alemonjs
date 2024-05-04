60388916dd49cc790d84976127439d97180ddc55
ningmengchongshui <916415899@qq.com>
2024-05-04 15:51:10 +0800

    fix: 错误写法

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,6 +1,3 @@
-const args = [...process.argv].slice(2)
-const index = args.indexOf('--run')
-const main = argv[index + 1]
 const argv = [...process.argv].slice(4)
 const BotName = 'alemonb'
 const findArgs = argv.find(item => item.startsWith('@'))
@@ -33,7 +30,7 @@ console.log('------------------------------------------')
 module.exports = {
   apps: apps.map(app => ({
     name: app.name,
-    script: main ?? 'node_modules/alemonjs/bin/index.js',
+    script: 'node_modules/alemonjs/bin/index.js',
     // exec_mode: 'cluster',
     args: app.args,
     error_file: `./logs/${app.name}/err.log`,
