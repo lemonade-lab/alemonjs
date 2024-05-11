f427cfd1027591dc395ba5021a9beda48fb84c04
ningmengchongshui <916415899@qq.com>
2024-05-11 20:15:51 +0800

    1.2.2

--- src/core/koa/middleware.ts
+++ src/core/koa/middleware.ts
@@ -1,12 +1,8 @@
 import koabodyparser from 'koa-bodyparser'
 import koacors from 'koa2-cors'
 import koarouter from 'koa-router'
-import koastatic from 'koa-static'
-import koawebsocket from 'koa-websocket'
 import koa from 'koa'
-export const AStatic = koastatic
-export const ABodyParser = koabodyparser
 export const ARouter = koarouter
+export const ABodyParser = koabodyparser
 export const ACors = koacors
-export const AWebSocket = koawebsocket
 export const AKoa = koa
