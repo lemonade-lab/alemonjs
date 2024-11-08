a761e853f05f269c75cbc44c96d880e015aa68cc
ningmengchongshui <ningmengchongshui@gmail.com>
2024-11-09 05:03:17 +0800

    feat: gui

--- packages/gui/src/index.ts
+++ packages/gui/src/index.ts
@@ -2,7 +2,7 @@ import { defineBot, getConfig, OnProcessor, Text, useParse } from 'alemonjs'
 import { WebSocketServer } from 'ws'
 import Koa from 'koa'
 import KoaStatic from 'koa-static'
-import Router from 'koa-router'
+// import Router from 'koa-router'
 import { mkdirSync, writeFileSync } from 'fs'
 import { join } from 'path'
 
@@ -12,18 +12,18 @@ import { join } from 'path'
  */
 const createServer = (port: number) => {
   const app = new Koa()
-  const router = new Router()
-  router.post('/update/user', async ctx => {
-    ctx.body = {
-      code: 200
-    }
-  })
+  // const router = new Router()
+  // router.post('/update/user', async ctx => {
+  //   ctx.body = {
+  //     code: 200
+  //   }
+  // })
   const dir = join(process.cwd(), 'public')
   mkdirSync(join(dir, 'file'), { recursive: true })
   // 暴露public文件夹
   app.use(KoaStatic(dir))
   // routes
-  app.use(router.routes())
+  // app.use(router.routes())
   return app.listen(port, () => {
     console.log(`gui server start at port ${port}`)
   })
@@ -44,6 +44,7 @@ export default defineBot(() => {
   let client = null
   //
   const onMessage = event => {
+    const txt = event.MsgBody.find((item: any) => item.t == 'text')
     const e = {
       // 事件类型
       Platform: event.Platform,
@@ -52,9 +53,9 @@ export default defineBot(() => {
       // 子频道
       ChannelId: event.ChannelId,
       // 是否是主人
-      IsMaster: event.IsMaster,
+      IsMaster: event.IsMaster == 0 ? false : true,
       // 用户ID
-      UserId: event.ChannelId,
+      UserId: event.UserId,
       // 用户名
       UserName: event.UserName,
       // 用户头像
@@ -62,7 +63,7 @@ export default defineBot(() => {
       // 格式化数据
       MsgId: event.MsgId,
       // 用户消息
-      Megs: [Text(event)],
+      Megs: [Text(txt.d)],
       // 用户openId
       OpenID: event.OpenID,
       // 创建时间
@@ -89,13 +90,13 @@ export default defineBot(() => {
     console.log('gui connection')
     //
     client = ws
-
     // 处理消息事件
     ws.on('message', (message: string) => {
       const event = JSON.parse(message)
       if (event.t == 'send_message') {
-        const txt = event.d.find((item: any) => item.t == 'text')
-        onMessage(txt.d)
+        onMessage(event.d)
+      } else if (event.t == 'send_private_message') {
+        // onMessage(event.d)
       } else {
         console.log('未知消息', event)
       }
@@ -111,47 +112,9 @@ export default defineBot(() => {
    * @param data
    * @returns
    */
-  const SendData = (data: any) => {
-    return client.send(JSON.stringify(data))
-  }
+  const SendData = (data: any) => client.send(JSON.stringify(data))
 
-  /**
-   *
-   * @param txt
-   */
-  const sendMessage = (txt: string) => {
-    return SendData({
-      t: 'send_message',
-      d: [
-        {
-          t: 'text',
-          d: txt
-        }
-      ]
-    })
-  }
-
-  /**
-   *
-   * @param url
-   */
-  const sendImage = (url: string) => {
-    return SendData({
-      t: 'send_message',
-      d: [
-        {
-          t: 'image',
-          d: {
-            url: url
-          }
-        }
-      ]
-    })
-  }
-
-  /**
-   *
-   */
+  //
   return {
     api: {
       use: {
@@ -160,7 +123,21 @@ export default defineBot(() => {
           console.log(event)
           const content = useParse(val, 'Text')
           if (content) {
-            return Promise.all([content].map(item => sendMessage(item)))
+            return Promise.all(
+              [content].map(item =>
+                SendData({
+                  t: 'send_message',
+                  d: {
+                    MsgBody: [
+                      {
+                        t: 'text',
+                        d: item
+                      }
+                    ]
+                  }
+                })
+              )
+            )
           }
           const images = useParse(val, 'Image')
           if (images) {
@@ -168,7 +145,19 @@ export default defineBot(() => {
               images.map(async item => {
                 const url = `/file/${Date.now()}.png`
                 writeFileSync(join(process.cwd(), 'public', url), item, 'utf-8')
-                return sendImage(url)
+                return SendData({
+                  t: 'send_message',
+                  d: {
+                    MsgBody: [
+                      {
+                        t: 'image',
+                        d: {
+                          url: url
+                        }
+                      }
+                    ]
+                  }
+                })
               })
             )
           }
