f1d7ae925cbcc39858740ac9d7d3940702da5201
ningmengchongshui <ningmengchongshui@gmail.com>
2026-03-27 14:56:40 +0800

    update: tp

--- packages/create-alemonjs/template/alemonjs/pm2.config.cjs
+++ packages/create-alemonjs/template/alemonjs/pm2.config.cjs
@@ -1,14 +1,19 @@
 const fs = require('fs');
 const yaml = require('yaml');
 
-// Read and parse the YAML configuration file
-const data = fs.readFileSync('./alemon.config.yaml', 'utf8');
-const config = yaml.parse(data);
-
-// Extracting PM2 configuration
-const pm2 = config?.pm2 ?? {};
+let pm2 = null;
+try {
+  // Read and parse the YAML configuration file
+  const data = fs.readFileSync('./alemon.config.yaml', 'utf8');
+  const config = yaml.parse(data);
+  pm2 = config?.pm2 ?? {};
+} catch (error) {
+  console.error('Error parsing YAML configuration:', error);
+}
 
 /**
  * @type {{ apps: import("pm2").StartOptions[] }}
  */
-module.exports = pm2;
+module.exports = pm2 || {
+  apps: []
+};
